2e4bf3736c4e77c42e79ac296f2ae3e432178fe0
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2022-01-24 10:46:03 +0100

    feat: migration create table stats

---
+++ project/src/database/migrations/1643017395726-CreateStatsEntity.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateStatsEntity1643017395726 implements MigrationInterface {
+    name = 'CreateStatsEntity1643017395726'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "stats" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL, "updated_at" TIMESTAMP NOT NULL, "deleted_at" TIMESTAMP, "raw_data" jsonb NOT NULL, "information" jsonb NOT NULL, "country" character varying(50), "stats" character varying(10), "startDateRange" date, "endDateRange" date, CONSTRAINT "PK_c76e93dfef28ba9b6942f578ab1" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "stats"`);
+    }
+
+}
